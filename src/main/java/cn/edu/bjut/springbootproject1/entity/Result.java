e446223e01b199116c4c03c93a3f456452f2766e
panshengqiu <2380387350@qq.com>
2024-03-12 16:32:21 +0800

    commit and push by panshengqiu.

--- src/main/java/cn/edu/bjut/springbootproject1/entity/Result.java
+++ src/main/java/cn/edu/bjut/springbootproject1/entity/Result.java
@@ -4,10 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+
 public class Result {
     private Integer code;//响应码，1 代表成功; 0 代表失败
     private String msg;  //响应信息 描述字符串
