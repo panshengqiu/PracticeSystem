4c4b9cfe12232088a3cd909773f9640cec2b762a
刘欣欣 <2495685455@qq.com>
2024-03-15 11:25:03 +0800

    修改
    Signed-off-by: 刘欣欣 <2495685455@qq.com>

--- src/test/java/cn/edu/bjut/springbootproject1/SpringbootProject1ApplicationTests.java
+++ src/test/java/cn/edu/bjut/springbootproject1/SpringbootProject1ApplicationTests.java
@@ -1,7 +1,4 @@
 package cn.edu.bjut.springbootproject1;
-
-import cn.edu.bjut.springbootproject1.entity.User;
-import cn.edu.bjut.springbootproject1.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,25 +7,10 @@ import java.util.List;
 
 @SpringBootTest // springboot整合单元测试的注解
 class SpringbootProject1ApplicationTests {
-    @Autowired
-    private UserMapper userMapper;
-    @Test
-    void contextLoads() {
-        System.out.println("hello world");
-    }
 
-    @Test
-    public void testListUser(){
-        List<User> userList = userMapper.list();
-        userList.stream().forEach(user -> {
-            System.out.println(user);
-        });
-    }
 
-    @Test
-    public void test(){
-        System.out.println("潘哥能看见吗");
-    }
+
+
 }
 
 
